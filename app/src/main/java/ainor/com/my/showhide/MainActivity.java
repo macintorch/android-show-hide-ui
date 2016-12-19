2811a769e9cd545168fb419e4119f524e9e24af8
Ainor Syahrizal <macintorch@gmail.com>
2016-12-19 22:53:04 +0800

    add show n hide function

--- app/src/main/java/ainor/com/my/showhide/MainActivity.java
+++ app/src/main/java/ainor/com/my/showhide/MainActivity.java
@@ -2,12 +2,36 @@ package ainor.com.my.showhide;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button showButton;
+    Button hideButton;
+    TextView textView;
+
+    public void showText (View view) {
+        textView.setVisibility(View.VISIBLE);
+        Log.i("Show Button: ", "Pressed");
+    }
+
+    public void hideText (View view) {
+        textView.setVisibility(View.INVISIBLE);
+
+        Log.i("Hide Button: ", "Pressed");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        showButton = (Button) findViewById(R.id.showButton);
+        hideButton = (Button) findViewById(R.id.hideButton);
+        textView = (TextView) findViewById(R.id.textView);
+
     }
 }
